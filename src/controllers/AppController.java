9b0e1e35bbde66da1402351c5cf24cb7e863fcbd
Alex <Proalexxxl@gmail.com>
2021-11-22 20:52:08 +0200

    Input client/employee

--- src/controllers/AppController.java
+++ src/controllers/AppController.java
@@ -1,8 +1,17 @@
 package controllers;
 
+import java.util.Scanner;
+
 public class AppController {
 
+    String subject;
+
     public void subjectChoice(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите субъекта (клиент/сотрудник): ");
+        subject = sc.nextLine();
+
+        subject = subject.toLowerCase().trim();
 
     }
 }
