d084870f68e5743d131cc82537badcaf75fc375a
Alex <Proalexxxl@gmail.com>
2021-11-22 09:33:25 +0200

    implement interface and method

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -1,4 +1,17 @@
 package models;
 
-public class Employee {
+import base.Prize;
+import base.Salary;
+
+public class Employee implements Salary, Prize {
+
+    @Override
+    public double calcPrize(double salesAmount, double prizeRate) {
+        return salesAmount * prizeRate /100;
+    }
+
+    @Override
+    public double calcSalary(double hours, double hoursRate) {
+        return hours * hoursRate;
+    }
 }
