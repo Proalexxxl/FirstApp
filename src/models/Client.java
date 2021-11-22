458ed5c36da370f7b8e81c8bf5b886217d7f6702
Alex <Proalexxxl@gmail.com>
2021-11-22 09:30:46 +0200

    implement interface and method

--- src/models/Client.java
+++ src/models/Client.java
@@ -1,4 +1,17 @@
 package models;
 
-public class Client {
+import base.Discount;
+import base.Payment;
+
+public class Client implements Payment, Discount {
+
+    @Override
+    public double calcDiscount(double payment, double discountRate) {
+        return payment * discountRate / 100;
+    }
+
+    @Override
+    public double calcPayment(int quantity, double price) {
+        return quantity * price;
+    }
 }
