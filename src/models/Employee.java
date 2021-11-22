948db212f678e64c6670c1cdcbcf306f16b0a3f4
Alex <Proalexxxl@gmail.com>
2021-11-22 19:14:02 +0200

    Declaring variable, add getter and setter

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -5,6 +5,61 @@ import base.Salary;
 
 public class Employee implements Salary, Prize {
 
+    private String name;
+    private String post;
+    private double hours;
+    private double hoursRate;
+    private double salesAmount;
+    private double prizeRate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+
+    public double getHoursRate() {
+        return hoursRate;
+    }
+
+    public void setHoursRate(double hoursRate) {
+        this.hoursRate = hoursRate;
+    }
+
+    public double getSalesAmount() {
+        return salesAmount;
+    }
+
+    public void setSalesAmount(double salesAmount) {
+        this.salesAmount = salesAmount;
+    }
+
+    public double getPrizeRate() {
+        return prizeRate;
+    }
+
+    public void setPrizeRate(double prizeRate) {
+        this.prizeRate = prizeRate;
+    }
+
     @Override
     public double calcPrize(double salesAmount, double prizeRate) {
         return salesAmount * prizeRate /100;
