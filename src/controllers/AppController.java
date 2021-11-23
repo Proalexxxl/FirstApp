d71e919dbf4d1fff2c2dc0a3d4a5a6313fa93fcb
Alex <Proalexxxl@gmail.com>
2021-11-23 22:56:09 +0200

    Add data in AppController

--- src/controllers/AppController.java
+++ src/controllers/AppController.java
@@ -1,4 +1,30 @@
 package controllers;
 
+import java.util.Scanner;
+
 public class AppController {
+
+    String subject;
+    ClientController clientController;
+    EmployeeController employeeController;
+
+    public AppController(ClientController clientController, EmployeeController employeeController) {
+        this.clientController = clientController;
+        this.employeeController = employeeController;
+    }
+
+    public void subjectChoice(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите субъекта (клиент/сотрудник): ");
+        subject = sc.nextLine();
+
+        subject = subject.toLowerCase().trim();
+
+        switch (subject) {
+            case "клиент" -> clientController.runAppClient();
+            case "сотрудник" -> employeeController.runAppEmployee();
+            default -> System.out.println("Неверный ввод.");
+        }
+    }
 }
