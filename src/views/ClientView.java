e6ba405dcbd9ef104733fb70338c1c9abbb95938
Alex <Proalexxxl@gmail.com>
2021-11-23 09:38:43 +0200

    Declaring variables, create Object

--- src/views/ClientView.java
+++ src/views/ClientView.java
@@ -1,4 +1,21 @@
 package views;
 
+import utils.Validator;
+import models.Client;
+import java.util.Scanner;
+
 public class ClientView {
+
+    Client model;
+    String name;
+    int quantity;
+    double price;
+    Scanner scanner;
+
+    public void inputData() {
+        model = new Client();
+        scanner = new Scanner(System.in);
+
+
+    }
 }
