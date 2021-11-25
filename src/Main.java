c0f7d967c6235bc8189d4c536db8cee4076d913e
Alex <proalexxxl@gmail.com>
2021-11-25 19:44:39 +0200

    refactor main

--- src/Main.java
+++ src/Main.java
@@ -13,23 +13,16 @@ import views.EmployeeView;
 
 public class Main {
 
-    private static Client clientModel;
-    private static EmployeeView employeeView;
-    private static ClientView clientView;
-    private static ClientController clientController;
-    private static Employee employeeModel;
-    private static EmployeeController employeeController;
-    private static AppController controller;
-
     public static void main(String[] args) {
 
-        employeeModel = new Employee();
-        clientModel = new Client();
-        clientView = new ClientView(clientModel);
-        employeeView = new EmployeeView(employeeModel);
-        clientController = new ClientController(clientView, clientModel);
-        employeeController = new EmployeeController(employeeModel, employeeView);
-        controller = new AppController(clientController, employeeController);
+        Employee employeeModel = new Employee();
+        Client clientModel = new Client();
+        ClientView clientView = new ClientView(clientModel);
+        EmployeeView employeeView = new EmployeeView(employeeModel);
+        ClientController clientController = new ClientController(clientView, clientModel);
+        EmployeeController employeeController = new EmployeeController(employeeModel, employeeView);
+        AppController controller = new AppController(clientController, employeeController);
+
         controller.subjectChoice();
     }
 }
